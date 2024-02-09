ed1d5da82d2a408772aab2fe5e7cc85aa806f6c8
Mahesha D <149392974+MAHESHA-D@users.noreply.github.com>
2024-02-09 17:52:11 +0530

    Update Login.js

--- Login.js
+++ Login.js
@@ -1,22 +1,19 @@
-
 async function login() {
-    const username = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
+    console.log("username : "+email);
+    console.log('password : '+password);
 
-    const response = await fetch('http://localhost:5000/api/signin', { // here you have to give your database end point
+    const response = await fetch("http://localhost:8080/api/signin", { // here you have to give your database end point
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ username, password })
-    });
-
-    const result = await response.json();
-
-    if (result.success) {
-      alert(result.message);
-      // Redirect or perform other actions after successful login
-    } else {
-      alert(result.message);
+      body: JSON.stringify({ email, password })
+    })
+    .then((response)=>response.json())
+    .then((data)=> console.log(data));
+    if(response.status){
+      window.location.href="task.html";
     }
-  }
+  } 
